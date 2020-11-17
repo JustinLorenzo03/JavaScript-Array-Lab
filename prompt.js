999fafdbd774d25ef15afc5c318433de458e8822
Justin Lorenzo <justin.lorenzo6699@gmail.com>
2020-11-17 14:47:38 -0500

    added the next step

--- prompt.js
+++ prompt.js
@@ -1,3 +1,11 @@
 var songs = ['Righteous', 'Del Rancho', 'Gooba', 'Lucid Dreams', 'RockStar'];
 console.log(songs[0]);
 console.log(songs[2]);
+
+songs.pop();
+songs.push('Happy');
+
+var jamFour = songs[3];
+console.log(jamFour);
+songs.push('EastSide');
+
