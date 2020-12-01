56dc3308051c64e249d5333c31d3fe9512b49d17
Justin Lorenzo <justin.lorenzo6699@gmail.com>
2020-12-01 15:01:15 -0500

    finished exercise 3

--- numbers.js
+++ numbers.js
@@ -7,3 +7,14 @@ function numberDesend() {
 }
 
 //Exercise 3
+const movies = [
+    [ 'Rio 2', 'Scooby-Doo' ],
+    [ 'Fast and Furious', 'Flash' ],
+    [ 'Meteor Garden', 'Love Alarm' ]
+]
+console.log(movies[2][1]);
+arr[1][0] = 'Pele';
+
+for(let i = 0; i < movies.length; i++) {
+    console.log(`${i}: ${movies[i][i + 1]}`);
+}
